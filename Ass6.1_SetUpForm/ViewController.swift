83f4662433362e61307a5d12fcd4ce1b5dba15c9
PedroJavier <pedrojaviermg23@gmail.com>
2022-11-15 19:26:18 -0500

    Initial Commit

---
+++ Ass6.1_SetUpForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ass6.1_SetUpForm
+//
+//  Created by user228139 on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
